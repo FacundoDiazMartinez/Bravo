18f9c29eda4a736529fb11d7af0374f24ba6dda4
FacundoDiazMartinez <facundo_diaz_martinez@hotmail.com>
2016-11-08 10:21:34 -0300

    Update comprobante_constatar.rb

--- lib/bravo/comprobante_constatar.rb
+++ lib/bravo/comprobante_constatar.rb
@@ -24,7 +24,7 @@ module Bravo
     end
     
     def set_body
-      data_fields = {"CmpReq" => {cbte_modo: "CAI", cuit_emisor: "20267565393", pto_vta: 4002, cbte_tipo: 1, cbte_nro: 109, cbte_fch: "20131227", imp_total: "121.0" , cod_autorizacion: "63523178385550", doc_tipo_receptor: 80, doc_nro_receptor: "30628789661"}}
+      data_fields = {"CmpReq" => {cbte_modo: "CAI", cuit_emisor: "20315487898", pto_vta: 4002, cbte_tipo: 1, cbte_nro: 109, cbte_fch: "20131227", imp_total: "121.0" , cod_autorizacion: "63523178385550", doc_tipo_receptor: 80, doc_nro_receptor: "30628789661"}}
       body.merge!(data_fields)
       return body
     end
